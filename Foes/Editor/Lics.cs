5870c2ac9784851bced9569a5fe18e50573216eb
Jeroen Broks <facebook@tbbs.nl>
2022-02-28 22:48:58 +0100

    Lic text
    (Will be used by the exporter)

---
+++ Foes/Editor/Lics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarStory2_Foe_Editor {
+	static class Lics {
+
+		static string zl = @"When completely free of references to the story line or the Star Story universe (or even the Phantasar univers) you may use this file under terms of the zlib license";
+		static string pv = @"This file is part of the story line of Star Story or may be part of the Phantasar lore. Extraction of this project is NOT allowed without prior WRITTEN permission from Jeroen P. Broks";
+		
+		
+	}
+}
