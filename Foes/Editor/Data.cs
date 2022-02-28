0bb77b0a3c632ee562821279c051bef9eaeaa359
Jeroen Broks <facebook@tbbs.nl>
2022-02-28 23:30:05 +0100

    Data Search
    (temporarily used the TFT work directory in order to test if this worked... which it does. Since my true data dir is empty now, testing would normally be impossible)

---
+++ Foes/Editor/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrickyUnits;
+
+namespace StarStory2_Foe_Editor {
+	static class Data {
+
+		static Data() {
+			Debug.WriteLine("Initizing 'Data' class");
+			Dirry.InitAltDrives();
+		}
+
+		static public string Dir => Dirry.AD("Scyndi:Projects/Applications/Apollo/Games/The Fairy Tale Revamped");
+	}
+}
--- Foes/Editor/MainWindow.xaml.cs
+++ Foes/Editor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TrickyUnits;
 
-namespace WpfApp1 {
+namespace StarStory2_Foe_Editor {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            GetFoeList();
+        }
+
+        void GetFoeList() {
+            Debug.WriteLine("Renewing Foe List");
+            FoeList.Items.Clear();
+            var L = FileList.GetTree(Data.Dir);
+            foreach(var f in L) {
+                if (f.ToUpper() != "README.MD") FoeList.Items.Add(f);
+            }
         }
     }
 }
