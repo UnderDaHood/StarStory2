e0ca70069301b87e59f8ce619ee535e8efbfa28f
Jeroen Broks <facebook@tbbs.nl>
2022-03-01 11:01:29 +0100

    Preparational work for TextBox processing
    (Please note this is all under the hood code, I'll need later. There is currently NO VISUAL EFFECT AT ALL).

--- Foes/Editor/Data.cs
+++ Foes/Editor/Data.cs
@@ -41,10 +41,77 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using TrickyUnits;
 
 namespace StarStory2_Foe_Editor {
 	static class Data {
+
+
+		#region Base Record
+		public struct SS2FRec {
+			public readonly GINIE Data;
+			public bool Modified;
+			internal SS2FRec(string f) {
+				var mf = $"{Dir}/{f}";
+				Data = GINIE.FromFile(mf);
+				Data.AutoSaveSource = mf;
+				Modified = false;
+			}
+		}
+		static SortedDictionary<string, SS2FRec> Records = new SortedDictionary<string, SS2FRec>();
+
+		static SS2FRec GetRec(string f) {
+			string uf = f.ToUpper();
+			if (!Records.ContainsKey(uf)) {
+				Debug.WriteLine($"Getting data for not yet loaded record: {uf}!");
+				Records[uf] = new SS2FRec(f);
+			}
+			return Records[uf];
+		}
+		#endregion
+
+		struct RegRec {
+			readonly public string Category;
+			readonly public string VarName;
+			internal RegRec(string VN) { Category = "Meta"; VarName = VN; }
+			internal RegRec(string CT, string VN) { Category = CT; VarName = VN; }
+		}
+		static Dictionary<TextBox, RegRec> AutoTexBox = new Dictionary<TextBox, RegRec>();
+		static public void RegTextBox(TextBox TB,string VN) { AutoTexBox[TB] = new RegRec(VN); }
+		static public void RegTextBox(TextBox TB, string CT, string VN) { AutoTexBox[TB] = new RegRec(CT,VN); }
+
+		static bool DontChange = false;
+		static public void ActTextBox(TextBox TB) {
+			try {
+				if (Foe == "" || DontChange) return;
+				var T = AutoTexBox[TB];
+				var R = GetRec(Foe);
+				R.Data[T.Category, T.VarName] = TB.Text;
+			} catch(Exception e) {
+				Confirm.Annoy($"An error occurred!\n\n{e.Message}\n");
+			}
+		}
+
+		static public void UpdateTextBoxes() {
+			DontChange = true;
+			foreach(var IT in AutoTexBox) {
+				IT.Key.IsEnabled = Foe != "";
+				if (Foe != "") {
+					var R = GetRec(Foe);
+					IT.Key.Text = R.Data[IT.Value.Category, IT.Value.VarName];
+				}
+			}
+		}
+
+		static ListBox FoeList;
+		static public string Foe {
+			get {
+				if (FoeList.SelectedItem == null) return "";
+				return FoeList.SelectedItem.ToString();
+			}
+		}
+
 
 		static Data() {
 			Debug.WriteLine("Initizing 'Data' class");
@@ -52,5 +119,8 @@ namespace StarStory2_Foe_Editor {
 		}
 
 		static public string Dir => Dirry.AD("Scyndi:Projects/Applications/Apollo/Games/Star Story 2/Dev/Foes/Data");
+		static public void RegFoeList(ListBox FL) { FoeList = FL; }
+
+
 	}
 }
