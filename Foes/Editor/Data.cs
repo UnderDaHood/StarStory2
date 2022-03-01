163ed8c944cb2e8493290c36f5812134d344c005
Jeroen Broks <facebook@tbbs.nl>
2022-03-01 09:53:06 +0100

    To the actual work directory

--- Foes/Editor/Data.cs
+++ Foes/Editor/Data.cs
@@ -1,4 +1,41 @@
-﻿using System;
+// Lic:
+// **********************************************
+// 
+// Dev/Foes/Editor/Data.cs
+// (c) Jeroen Broks, 2022, All Rights Reserved.
+// 
+// This file contains material that is related
+// to a storyline that is which is strictly
+// copyrighted to Jeroen Broks.
+// 
+// This file may only be used in an unmodified
+// form with an unmodified version of the
+// software this file belongs to.
+// 
+// You may use this file for your study to see
+// how I solved certain things in the creation
+// of this project to see if you find valuable
+// leads for the creation of your own.
+// 
+// Mostly this file comes along with a project
+// that is for most part released under an
+// open source license and that means that if
+// you use that code with this file removed
+// from it, you can use it under that license.
+// Please check out the other files to find out
+// which license applies.
+// This file comes 'as-is' and in no possible
+// way the author can be held responsible of
+// any form of damages that may occur due to
+// the usage of this file
+// 
+// 
+// **********************************************
+// 
+// version: 22.03.01
+// EndLic
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -14,6 +51,6 @@ namespace StarStory2_Foe_Editor {
 			Dirry.InitAltDrives();
 		}
 
-		static public string Dir => Dirry.AD("Scyndi:Projects/Applications/Apollo/Games/The Fairy Tale Revamped");
+		static public string Dir => Dirry.AD("Scyndi:Projects/Applications/Apollo/Games/Star Story 2/Dev/Foes/Data");
 	}
 }
