ea00c11752b77d712a160a19f47d93eb19b4f280
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-05-21 17:00:14 +0200

    Sewer Generator
    Just a quick C# program I created in order to make sure the sewer template is copied in (almost) all layers. It's a kind of laziness, I know, but what works that works

---
+++ Sewer/Program.cs
@@ -0,0 +1,102 @@
+﻿#define AllesKlaar
+
+using NSKthura;
+using UseJCR6;
+using TrickyUnits;
+
+const int
+	w = 5,
+	h = 5,
+	mw = 3,
+	mh = 3;
+
+
+const string
+	ori = @"E:\Projects\Applications\Apollo\Games\Star Story 2\src\Tricky Story\Kthura\006_Sewers",
+#if AllesKlaar
+	tar = @"E:\Projects\Applications\Apollo\Games\Star Story 2\src\Tricky Story\Kthura\006_Sewers";
+#else
+	tar = @"E:\Projects\Applications\Apollo\Games\Star Story 2\src\Tricky Story\Kthura\006_Sewers_Kopie";
+#endif
+
+Kthura
+	TheMap;
+
+KthuraLayer L(int x,int y) {
+	var SL = $"AX{x}Y{y}";
+	if (!TheMap.Layers.ContainsKey(SL)) { TheMap.CreateLayer(SL); QCol.Doing("Created", SL); }
+	return TheMap.Layers[SL];
+}
+
+
+
+#region MAIN
+JCR6_zlib.Init();
+Kthura.LoadUnknown = true;
+
+QCol.DoingTab = 20;
+
+QCol.Doing("Loading", ori);
+TheMap = Kthura.Load(ori);
+for (int y = 1; y <= h; y++) {
+	for (int x = 1; x <= w; x++) {
+		var SL = $"AX{x}Y{y}";
+		var Lay = L(x, y);
+		if (Lay.LabelMap.ContainsKey("FROM_TEMPLATE")){
+			QCol.Doing("Cleaning", SL);
+			Lay.RemapLabels();
+			var Kill = Lay.LabelMap["FROM_TEMPLATE"].ToArray();
+			foreach (var victim in Kill) Lay.Objects.Remove(victim);
+		}
+		QCol.Doing("Cloning to", SL);
+		foreach(var obj in TheMap.Layers["TEMPLATE"].Objects) {
+			bool MagIk = true;
+			switch (obj.Tag) {
+				case "BN":
+					MagIk = !((y > 1) || (x == mw));
+					break;
+				case "BS":
+					MagIk = !((y < h) || (x == mw));
+					break;
+				case "BW":
+					MagIk = !((x > 1));// || (y == mh));
+					break;
+				case "BE":
+					MagIk = !((x < w));// || (y == mh));
+					break;					
+			}
+			if (MagIk) {
+				var taro = Lay.Clone(obj);
+				taro.Labels = "FROM_TEMPLATE";
+				switch (obj.Tag) {
+					case "GN":
+						if (y == 1) 
+						taro.Tag = $"GO_#002_Start";
+						else
+							taro.Tag = $"GO_AX{x}Y{y - 1}_StartS";
+						break;
+					case "GS":
+						if (y == h)
+							taro.Tag = $"GO_#001_Einde";
+						else
+							taro.Tag = $"GO_AX{x}Y{y + 1}_StartN";
+						break;
+					case "GW":
+						taro.Tag = $"GO_AX{x - 1}Y{y}_StartE";
+						break;
+					case "GE":
+						taro.Tag = $"GO_AX{x + 1}Y{y}_StartW";
+						break;
+				}
+			}
+
+		}
+	}
+}
+QCol.Doing("Saving", tar);
+KthuraSave.Save(TheMap, tar, "Store", "Jeroen P. Broks", "Don't use without permission");
+QCol.Cyan("Ok\n\n");
+Console.ResetColor();
+#endregion
+
+
