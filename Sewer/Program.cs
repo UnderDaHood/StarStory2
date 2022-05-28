520ce153ab53058322cf965076df5f197c58f0ba
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-05-28 23:40:41 +0200

    Update Program.cs

--- Sewer/Program.cs
+++ Sewer/Program.cs
@@ -1,4 +1,29 @@
-﻿#define AllesKlaar
+// Lic:
+// Sewer Generator
+// Well, a simple program that does what the name implies
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2022
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 22.05.21
+// EndLic
+#define AllesKlaar
 
 using NSKthura;
 using UseJCR6;
@@ -98,5 +123,3 @@ KthuraSave.Save(TheMap, tar, "Store", "Jeroen P. Broks", "Don't use without perm
 QCol.Cyan("Ok\n\n");
 Console.ResetColor();
 #endregion
-
-
